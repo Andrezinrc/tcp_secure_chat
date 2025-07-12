820eed1a140a546caceb14937795edd80ec1b82f
Andrezinrc <andrem.33333@gmail.com>
2025-07-12 22:10:45 +0000

    feat(client): implementa chat com threads de envio e recebimento via socket TCP

--- client.c
+++ client.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <time.h>
+#include <pthread.h>
 
 #define SERVER_IP "127.0.0.1" // IP
 #define PORT 8080 // porta
@@ -46,8 +47,7 @@ void* send_messages(void* arg) {
 
 int main() {
     struct sockaddr_in server_addr;
-    char* msg = "Hello World!";
-
+    
     // cria o socket TCP IPv4
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) {
@@ -66,9 +66,14 @@ int main() {
         exit(1);
     }
 
-    // envia a mensagem para o servidor
-    send(sockfd, msg, strlen(msg), 0);
-    
+    // cria threads
+    pthread_t recv_thread, send_thread;
+    pthread_create(&recv_thread, NULL, receive_messages, NULL);
+    pthread_create(&send_thread, NULL, send_messages, NULL);
+
+    // espera as threads
+    pthread_join(recv_thread, NULL);
+    pthread_join(send_thread, NULL);
     close(sockfd);
 
     return 0;
