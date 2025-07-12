99a39abbbe2083c53584879ca0a489e3bbe2d3bd
Andrezinrc <andrem.33333@gmail.com>
2025-07-12 19:14:24 +0000

    estrutura base do servidor TCP criada e aceitando conexões

---
+++ server.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int client_fd;
+char key[16];
+
+int main(){
+    int server_fd;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_size = sizeof(client_addr);
+
+    // cria socket TCP
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT); // define a porta
+    server_addr.sin_addr.s_addr = INADDR_ANY; // aceita qualquer IP
+
+    bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    listen(server_fd, 1); // espera conexões
+
+    printf("[Servidor] Aguardando conexão na porta %d...\n", PORT);
+    client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_size);  // aceita cliente
+    
+    printf("[Servidor] Cliente conectado!\n");
+    
+    return 0;
+}
